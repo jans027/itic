85ec836e19b5e0ff89720d9f7e184f3d991096a3
julian <87904137+jans027@users.noreply.github.com>
2023-02-13 14:49:12 -0500

    inicialisando proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Arggggggggggggggg</h1>
+    </div>
+  );
+}
+
+export default App;
