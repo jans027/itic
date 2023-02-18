cb511dbcfdf1111b72acb78dcb807e1d3091e924
julian <87904137+jans027@users.noreply.github.com>
2023-02-18 17:32:31 -0500

    stiles responsive banner

--- src/components/SliderBanner.jsx
+++ src/components/SliderBanner.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 // import Slider from "react-slick";
-import banner1 from '../images/banner-ppal.png';
+import largeImage from '../images/banner-ppal.png';
+import smallImage from '../images/banner_ppa_mobile.png'
 import { ContImgCarousel } from "../styles/SliderBanner";
 
 
 export default class SimpleSlider extends Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            screenWidth: window.innerWidth
+        };
+    }
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+    handleResize = () => {
+        this.setState({
+            screenWidth: window.innerWidth
+        });
+    };
+
+
+    render( ) {
+
+        // we get the value from the display and change the state
+        const { screenWidth } = this.state;
+
+
+
         const settings = {
             dots: true,
             infinite: true,
@@ -26,7 +54,7 @@ export default class SimpleSlider extends Component {
                         // marginTop: "-30px",
                     }}
                 >
-                    <ul style={{ marginTop: "0px", background:"#ECECEC" }}> {dots} </ul>
+                    <ul style={{ marginTop: "0px", background: "#ECECEC" }}> {dots} </ul>
                 </div>
             ),
         };
@@ -34,30 +62,30 @@ export default class SimpleSlider extends Component {
             <div>
                 <ContImgCarousel {...settings}>
                     <div>
-                        <img src={banner1} alt="" />
+                        <img src={screenWidth < 768 ? smallImage : largeImage} alt="Imagen" />
                         <span>
                             <h2>INSPECCIÓN</h2>
-                            <p>A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
+                            <p className="textCarrousel">A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
                             <br />
                             <br />
                             <p><a href="/">SOLICITAR INFORMACIÓN</a></p>
                         </span>
                     </div>
                     <div>
-                        <img src={banner1} alt="" />
+                    <img src={screenWidth < 768 ? smallImage : largeImage} alt="Imagen" />
                         <span>
                             <h2>INSPECCIÓN</h2>
-                            <p>A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
+                            <p className="textCarrousel">A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
                             <br />
                             <br />
                             <p><a href="/">SOLICITAR INFORMACIÓN</a></p>
                         </span>
                     </div>
                     <div>
-                        <img src={banner1} alt="" />
+                    <img src={screenWidth < 768 ? smallImage : largeImage} alt="Imagen" />
                         <span>
                             <h2>INSPECCIÓN</h2>
-                            <p>A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
+                            <p className="textCarrousel">A través de nuestros servicios de inspección, ponemos a disposición de los clientes toda nuestra experticia y experiencia técnica para ser sus ojos en el lugar del mundo que lo requieran y de esta manera garantizar la calidad en sus procesos y evitar pérdidas de tiempo y dinero.</p>
                             <br />
                             <br />
                             <p><a href="/">SOLICITAR INFORMACIÓN</a></p>
--- src/styles/SliderBanner.js
+++ src/styles/SliderBanner.js
@@ -9,18 +9,25 @@ export const ContImgCarousel = styled(Slider)`
     top: 100px;
     width: 100%;
     height: 500px;
-    min-height: 150px;
-    margin-bottom: 1rem;
+    /* min-height: 150px; */
+    margin-bottom: 10rem;
     div{
-        height: 400px;
+        width: 100%;
+        height: 500px;
         overflow: hidden;
         position: static;
+        /* border: 1px solid red;//............... */
+        @media(max-width:768px){
+            height: 500px;
+        }
         img{
             width: 100%;
             height: max-content;
-            object-fit: cover;
-            object-position: center right;
-            
+            object-fit:cover;
+            object-position: center center;
+            @media(max-width:768px){
+            height: 500px;
+        }
         }
         span{
             position: fixed;
@@ -30,17 +37,31 @@ export const ContImgCarousel = styled(Slider)`
             width: 450px;
             @media (min-width:1440px) {
                 top :15%;
-                }
+            }
+            @media(max-width:650px){
+                top :35%;
+                    
+            }
             h2{
                 font-size: 65px;
+                @media(max-width:650px){
+                    font-size: 45px;
+                }
             }
-            p{
+            .textCarrousel{
                 transition: 3s;
                 font-size: 1.5rem;
                 @media (min-width:1445px) {
                     font-size: 1vw;
+                    width: 150px;
                 }
-                a{
+                @media(max-width:650px){
+                    display: none;
+                    
+                }
+                
+            }
+            a{
                     margin-top: 100px;
                     padding:  10px 85px;
                     background-color:var(--color4);
@@ -50,6 +71,10 @@ export const ContImgCarousel = styled(Slider)`
                     font-weight: 700;
                     text-decoration:none;
                     border: 1px solid var(--color4);
+                    @media(max-width:650px){
+                        font-size: 15px;
+                        padding:  10px 40px;
+                    }
                     &:hover{
                         color: var(--color1);
                     }
@@ -58,12 +83,9 @@ export const ContImgCarousel = styled(Slider)`
                         border: none;
                     }
                 }
-            }
         }
     }
 `
-
-
 
 
 
